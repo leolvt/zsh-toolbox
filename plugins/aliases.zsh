a076be07603db4f5fe96594dfe8ba9e83dc5afc7
Leonardo V. Teixeira <leolvt@gmail.com>
2019-08-31 17:30:38 -0400

    Add aliases for 'ls'

--- plugins/aliases.zsh
+++ plugins/aliases.zsh
@@ -1,1 +1,9 @@
 alias ..="cd .."
+
+## Colorize the ls output, use better units (e.g MB), list folders first
+alias ls='ls --color=auto -h --group-directories-first'
+
+## Use a long listing format ##
+alias ll='ls -l'
+alias la='ls -la'
+alias l.='ls -d .* --color=auto'
